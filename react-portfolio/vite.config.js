e05e21a9f2cb312eeca26bb2c27a969472f0a9ca
tmh6059 <timhend2001@gmail.com>
2024-01-30 00:02:43 -0500

    pages fix

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio",
+  base: "/tmh6059.github.io",
   plugins: [react()],
   css: {
     modules: {
